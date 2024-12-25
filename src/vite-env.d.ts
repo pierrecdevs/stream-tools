9e5d262b6f886266edb44c2af68c54c0d08557ce
Pierre C <102039463+pierrecdevs@users.noreply.github.com>
2024-12-24 21:45:07 -0500

    added more env variables for twitch

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,13 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_OBS_WEBSOCKET_HOST: string
-  readonly VITE_OBS_WEBSOCKET_PORT: number
-  readonly VITE_OBS_WEBSOCKET_PASSWORD: string
-  // more env variables...
+  readonly VITE_OBS_WEBSOCKET_HOST: string;
+  readonly VITE_OBS_WEBSOCKET_PORT: number;
+  readonly VITE_OBS_WEBSOCKET_PASSWORD: string;
+  readonly VITE_TWITCH_SCERET: string;
+  readonly VITE_TWITCH_USERNAME: string;
+  readonly VITE_TWITCH_CLIENT_ID: string;
+  readonly VITE_TWITCH_OAUTH_TOKEN: string;
+  readonly VITE_TWITCH_REDIRECT_URI: string;
 }
 
 interface ImportMeta {
